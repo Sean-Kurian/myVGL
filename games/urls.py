1774586434fa60134072ba47a0b3794db483eaa0
Jackson Ashby <116326932+J-Ashby17@users.noreply.github.com>
2023-04-30 21:51:20 -0400

    3 change name of site from django rest with react what shows up when you hover on the tab to myvideogamelist (#4)
    * intial commit
    
    * remove jackson from readme
    
    * Changed tab name to MyVideoGameList in index.html
    
    * created admin server and a super user 'jacks' in the urls.py file

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib import admin 
 from . import views
 
 urlpatterns = [
     path('api/game/', views.GameListView.as_view()),
+    path('admin/', admin.site.urls),
 ]
